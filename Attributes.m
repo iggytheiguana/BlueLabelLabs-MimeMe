acc627537ce748a620b963f254b71e732b3214f8
Bobby Gill <bobby.gill@gmail.com>
2012-08-10 17:49:18 -0400

    Added 3 properties to the ApplicationSettings to convey gem costs for certain actions

--- Attributes.m
+++ Attributes.m
@@ -141,3 +141,6 @@ NSString* const ISPUBLIC                =@"ispublic";
 NSString* const ISFAVORITE              =@"isfavorite";
 NSString* const HASANSWERED             =@"hasanswered";
 NSString* const MIMEID                  =@"mimeid";
+NSString* const GEMS_FOR_CLUE           =@"gems_for_clue";
+NSString* const GEMS_FOR_NEW_WORD       =@"gems_for_new_word";
+NSString* const GEMS_FOR_GETTING_NEW_WORDS  =@"gems_for_getting_new_words";
